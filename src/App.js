6b526897c19dad0aff1e82e9f443471bdf16dfe4
lord-JesusChrist <obide1@yahoo.com>
2022-05-02 15:46:11 +0100

    adding functions

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+function Test()
+{
+  return <h1>React is bae</h1>
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Test />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload
         </p>
+
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -17,6 +25,7 @@ function App() {
         >
           Learn React
         </a>
+        <Test />
       </header>
     </div>
   );
